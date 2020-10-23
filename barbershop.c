28c4e075472681941328f1adca1fd262d312434a
João Hirasawa <joao@estudante.ufscar.br>
2020-10-23 20:45:09 -0300

    arrumeiiiiiiiii

--- barbershop.c
+++ barbershop.c
@@ -13,13 +13,13 @@ void * customerThread(void * args);
 sem_t sofa, customer1, customer2, payment, receipt;
 pthread_mutex_t mutex1, mutex2;
 
-int customers = 0;
+int customers = 0, counter = 0;
 struct Queue *queue_1, *queue_2;
 
 int main() {
     int i;
-    pthread_t barbers[BARBERS];
-    pthread_t customers[CUSTOMERS];
+    pthread_t barbers[BARBERS], customers[CUSTOMERS];
+    unsigned long barbers_id[BARBERS], customers_id[CUSTOMERS];
     
     queue_1 = createQueue(CUSTOMERS);
     queue_2 = createQueue(BARBERS);
@@ -34,14 +34,20 @@ int main() {
     sem_init(&receipt, 0, 0);
 
     for(i = 0; i < BARBERS; i++) {
-        pthread_create(&barbers[i], NULL, barberThread, NULL);
+        barbers_id[i] = (i+1)*100;
+        if(pthread_create(&barbers[i], NULL, barberThread, (void*)&barbers_id[i]) != 0) {
+            perror("Problema na criacao da thread");
+            exit(EXIT_FAILURE);
+        }
     }
 
     for(i = 0; i < CUSTOMERS; i++) {
-        pthread_create(&customers[i], NULL, customerThread, NULL);
+        customers_id[i] = i+1;
+        if(pthread_create(&customers[i], NULL, customerThread, (void*)&customers_id[i]) != 0) {
+            perror("Problema na criacao da thread");
+            exit(EXIT_FAILURE);
+        }
     }
-
-    sleep(200);
 
     for(i = 0; i < BARBERS; i++) {
         if(pthread_join(barbers[i], NULL) != 0) {
@@ -70,8 +76,9 @@ int main() {
 }
 
 void * barberThread(void * args) {
-    unsigned long id = 0;//*((unsigned long*) args);
+    unsigned long id = *((unsigned long*) args);
     while(1) {
+        printf("--%lu pronto\n", id);
         sem_t *sem1, *sem2;
         sem_wait(&customer1);
 
@@ -86,31 +93,31 @@ void * barberThread(void * args) {
         pthread_mutex_lock(&mutex2);
             sem2 = pop(queue_2);
             sem_post(sem2);
-            printf("Veio pra cadeirinha\n");
+            printf("--%lu trouxe cliente\n", id);
         pthread_mutex_unlock(&mutex2);
 
-        printf("Cortando\n");
+        printf("--%lu cortou\n", id);
         // Corta cabelo
         sleep(2);
 
         sem_wait(&payment);
         sem_post(&receipt);
-        printf("Pagamento recebido\n");
+        printf("--%lu deu recibo\n", id);
     }
 }
 
 void * customerThread(void * args) {
-    unsigned long id = customers;//*((unsigned long*) args);
+    unsigned long id = *((unsigned long*) args);
     sem_t sem1, sem2;
     sem_init(&sem1, 0, 0);
     sem_init(&sem2, 0, 0);
 
     pthread_mutex_lock(&mutex1);
-        printf("%lu Entrei na loja...\n", id);
+        printf("%lu ENTROU\n", id);
         if(customers == 20) {
             pthread_mutex_unlock(&mutex1);
             printf("%lu sifude loja cheia\n", id);
-            exit(EXIT_FAILURE);
+            return 1;
         }
         customers++;
         push(queue_1, &sem1);
@@ -121,7 +128,7 @@ void * customerThread(void * args) {
     sem_wait(&sem1);
 
     sem_wait(&sofa);
-        printf("%lu Sentei no sofa\n", id);
+        printf("%lu SENTOU\n", id);
         sem_post(&sem1);
         pthread_mutex_lock(&mutex2);
             push(queue_2, &sem2);
@@ -132,10 +139,10 @@ void * customerThread(void * args) {
 
     pthread_mutex_lock(&mutex1);
         sem_post(&payment);
-        printf("%lu Enviei o pagamento!\n", id);
+        printf("%lu PAGOU\n", id);
         sem_wait(&receipt);
-        printf("%lu Recebi o recibo!\n", id);
+        printf("%lu RECEBEU\n", id);
         customers--;
     pthread_mutex_unlock(&mutex1);
-    exit(EXIT_SUCCESS);
+    return 0;
 }
