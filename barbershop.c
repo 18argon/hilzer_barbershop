fe6fe6c6e15783846d4f1d8b484aff6770714f9c
Pabolo Pires <pabolo18@gmail.com>
2020-10-24 16:33:12 -0300

    Arrumando formatação

--- barbershop.c
+++ barbershop.c
@@ -7,8 +7,8 @@
 #define BARBERS 3
 #define CUSTOMERS 20
 
-void * barberThread(void * args);
-void * customerThread(void * args);
+void *barberThread(void *args);
+void *customerThread(void *args);
 
 sem_t sofa, customer1, customer2, payment, receipt;
 pthread_mutex_t mutex1, mutex2;
@@ -16,11 +16,12 @@ pthread_mutex_t mutex1, mutex2;
 int customers = 0, counter = 0;
 struct Queue *queue_1, *queue_2;
 
-int main() {
+int main()
+{
     int i;
     pthread_t barbers[BARBERS], customers[CUSTOMERS];
     unsigned long barbers_id[BARBERS], customers_id[CUSTOMERS];
-    
+
     queue_1 = createQueue(CUSTOMERS);
     queue_2 = createQueue(BARBERS);
 
@@ -33,24 +34,30 @@ int main() {
     sem_init(&payment, 0, 0);
     sem_init(&receipt, 0, 0);
 
-    for(i = 0; i < BARBERS; i++) {
-        barbers_id[i] = (i+1)*100;
-        if(pthread_create(&barbers[i], NULL, barberThread, (void*)&barbers_id[i]) != 0) {
+    for (i = 0; i < BARBERS; i++)
+    {
+        barbers_id[i] = (i + 1) * 100;
+        if (pthread_create(&barbers[i], NULL, barberThread, (void *)&barbers_id[i]) != 0)
+        {
             perror("Problema na criacao da thread");
             exit(EXIT_FAILURE);
         }
     }
 
-    for(i = 0; i < CUSTOMERS; i++) {
-        customers_id[i] = i+1;
-        if(pthread_create(&customers[i], NULL, customerThread, (void*)&customers_id[i]) != 0) {
+    for (i = 0; i < CUSTOMERS; i++)
+    {
+        customers_id[i] = i + 1;
+        if (pthread_create(&customers[i], NULL, customerThread, (void *)&customers_id[i]) != 0)
+        {
             perror("Problema na criacao da thread");
             exit(EXIT_FAILURE);
         }
     }
 
-    for(i = 0; i < CUSTOMERS; i++) {
-        if(pthread_join(customers[i], NULL) != 0) {
+    for (i = 0; i < CUSTOMERS; i++)
+    {
+        if (pthread_join(customers[i], NULL) != 0)
+        {
             perror("Problema no join");
             exit(EXIT_FAILURE);
         }
@@ -59,11 +66,11 @@ int main() {
     sleep(1);
     printf("Todos os clientes foram atendidos");
 
-    for(i = 0; i < BARBERS; i++) {
+    for (i = 0; i < BARBERS; i++)
+    {
         printf("--%lu encerrou o seu expediente\n", barbers_id[i]);
         pthread_cancel(barbers[i]);
     }
-
 
     sem_destroy(&sofa);
     sem_destroy(&customer1);
@@ -77,25 +84,27 @@ int main() {
     return 0;
 }
 
-void * barberThread(void * args) {
-    unsigned long id = *((unsigned long*) args);
-    while(1) {
+void *barberThread(void *args)
+{
+    unsigned long id = *((unsigned long *)args);
+    while (1)
+    {
         printf("--%lu pronto\n", id);
         sem_t *sem1, *sem2;
         sem_wait(&customer1);
 
         pthread_mutex_lock(&mutex1);
-            sem1 = pop(queue_1);
-            sem_post(sem1);
-            sem_wait(sem1);
+        sem1 = pop(queue_1);
+        sem_post(sem1);
+        sem_wait(sem1);
         pthread_mutex_unlock(&mutex1);
         sem_post(sem1);
 
         sem_wait(&customer2);
         pthread_mutex_lock(&mutex2);
-            sem2 = pop(queue_2);
-            sem_post(sem2);
-            printf("--%lu trouxe cliente\n", id);
+        sem2 = pop(queue_2);
+        sem_post(sem2);
+        printf("--%lu trouxe cliente\n", id);
         pthread_mutex_unlock(&mutex2);
 
         printf("--%lu cortou\n", id);
@@ -108,43 +117,45 @@ void * barberThread(void * args) {
     }
 }
 
-void * customerThread(void * args) {
-    unsigned long id = *((unsigned long*) args);
+void *customerThread(void *args)
+{
+    unsigned long id = *((unsigned long *)args);
     sem_t sem1, sem2;
     sem_init(&sem1, 0, 0);
     sem_init(&sem2, 0, 0);
 
     pthread_mutex_lock(&mutex1);
-        printf("%lu ENTROU\n", id);
-        if(customers == 20) {
-            pthread_mutex_unlock(&mutex1);
-            printf("%lu sifude loja cheia\n", id);
-            return 1;
-        }
-        customers++;
-        push(queue_1, &sem1);
+    printf("%lu ENTROU\n", id);
+    if (customers == 20)
+    {
+        pthread_mutex_unlock(&mutex1);
+        printf("%lu sifude loja cheia\n", id);
+        return 1;
+    }
+    customers++;
+    push(queue_1, &sem1);
     pthread_mutex_unlock(&mutex1);
-    
+
     sem_post(&customer1);
 
     sem_wait(&sem1);
 
     sem_wait(&sofa);
-        printf("%lu SENTOU\n", id);
-        sem_post(&sem1);
-        pthread_mutex_lock(&mutex2);
-            push(queue_2, &sem2);
-        pthread_mutex_unlock(&mutex2);
-        sem_post(&customer2);
-        sem_wait(&sem2);
+    printf("%lu SENTOU\n", id);
+    sem_post(&sem1);
+    pthread_mutex_lock(&mutex2);
+    push(queue_2, &sem2);
+    pthread_mutex_unlock(&mutex2);
+    sem_post(&customer2);
+    sem_wait(&sem2);
     sem_post(&sofa);
 
     pthread_mutex_lock(&mutex1);
-        sem_post(&payment);
-        printf("%lu PAGOU\n", id);
-        sem_wait(&receipt);
-        printf("%lu RECEBEU\n", id);
-        customers--;
+    sem_post(&payment);
+    printf("%lu PAGOU\n", id);
+    sem_wait(&receipt);
+    printf("%lu RECEBEU\n", id);
+    customers--;
     pthread_mutex_unlock(&mutex1);
     return 0;
 }
--- queue.c
+++ queue.c
@@ -6,69 +6,68 @@
 #include <semaphore.h>
 
 // A structure to represent a queue
-struct Queue {
+struct Queue
+{
     int front, rear, size;
     unsigned capacity;
-    sem_t** array;
+    sem_t **array;
 };
 
 // function to create a queue
 // of given capacity.
 // It initializes size of queue as 0
-struct Queue* createQueue(unsigned capacity)
+struct Queue *createQueue(unsigned capacity)
 {
-    struct Queue* queue = (struct Queue*)malloc(
+    struct Queue *queue = (struct Queue *)malloc(
         sizeof(struct Queue));
     queue->capacity = capacity;
     queue->front = queue->size = 0;
 
     // This is important, see the enqueue
     queue->rear = capacity - 1;
-    queue->array = (sem_t**)malloc(
+    queue->array = (sem_t **)malloc(
         queue->capacity * sizeof(sem_t *));
     return queue;
 }
 
 // Queue is full when size becomes
 // equal to the capacity
-int isFull(struct Queue* queue)
+int isFull(struct Queue *queue)
 {
     return (queue->size == queue->capacity);
 }
 
 // Queue is empty when size is 0
-int isEmpty(struct Queue* queue)
+int isEmpty(struct Queue *queue)
 {
     return (queue->size == 0);
 }
 
 // Function to add an item to the queue.
 // It changes rear and size
-void push(struct Queue* queue, sem_t *item)
+void push(struct Queue *queue, sem_t *item)
 {
     if (isFull(queue))
         return;
-    queue->rear = (queue->rear + 1)
-                  % queue->capacity;
+    queue->rear = (queue->rear + 1) % queue->capacity;
     queue->array[queue->rear] = item;
     queue->size = queue->size + 1;
 }
 
 // Function to remove an item from queue.
 // It changes front and size
-sem_t* pop(struct Queue* queue)
+sem_t *pop(struct Queue *queue)
 {
     if (isEmpty(queue))
         return NULL;
-    sem_t* item = queue->array[queue->front];
-    queue->front = (queue->front + 1)
-                   % queue->capacity;
+    sem_t *item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
     queue->size = queue->size - 1;
     return item;
 }
 
 // Function to get front of queue
-sem_t* front(struct Queue* queue)
+sem_t *front(struct Queue *queue)
 {
     if (isEmpty(queue))
         return NULL;
@@ -76,7 +75,7 @@ sem_t* front(struct Queue* queue)
 }
 
 // Function to get rear of queue
-sem_t* rear(struct Queue* queue)
+sem_t *rear(struct Queue *queue)
 {
     if (isEmpty(queue))
         return NULL;
